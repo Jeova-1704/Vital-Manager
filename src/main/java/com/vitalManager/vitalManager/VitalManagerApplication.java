cea0856914b85f26a2ebda0a774fce1785ba5ec8
Jeova BL <jeovab115@gmail.com>
2024-06-19 23:49:18 -0300

    initial archicheture

---
+++ src/main/java/com/vitalManager/vitalManager/VitalManagerApplication.java
@@ -0,0 +1,13 @@
+package com.vitalManager.vitalManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalManagerApplication.class, args);
+	}
+
+}
