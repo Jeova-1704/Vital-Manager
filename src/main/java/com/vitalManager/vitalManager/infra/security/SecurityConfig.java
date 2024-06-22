00d347eaa85272c25ea363dee1a4911ff9203eb0
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-06-21 22:49:29 -0300

    fix: rotas de autenticacao

--- src/main/java/com/vitalManager/vitalManager/infra/security/SecurityConfig.java
+++ src/main/java/com/vitalManager/vitalManager/infra/security/SecurityConfig.java
@@ -31,7 +31,6 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.POST, "usuario/").permitAll()
                         .anyRequest().authenticated()
                 ).addFilterBefore(Filter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
