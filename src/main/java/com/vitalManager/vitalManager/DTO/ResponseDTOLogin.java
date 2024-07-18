c9f3c4068de69bcaead142b13008de0ad3bd90c8
Jeova BL <jeovab115@gmail.com>
2024-07-18 15:29:21 -0300

    fix<respondeLoginDTO>: retorna o id do prontuário quando faz login

--- src/main/java/com/vitalManager/vitalManager/DTO/ResponseDTOLogin.java
+++ src/main/java/com/vitalManager/vitalManager/DTO/ResponseDTOLogin.java
@@ -1,5 +1,5 @@
 package com.vitalManager.vitalManager.DTO;
 
-public record ResponseDTOLogin(String nome, String tipo, Integer id, String token, Integer id_fk) {
+public record ResponseDTOLogin(String nome, String tipo, Integer id, String token, Integer id_fk, Integer prontuario_id) {
 }
 
